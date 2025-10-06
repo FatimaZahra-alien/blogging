eefe7b28ee3d52e7025a2721660b5e4d612a368e
FatimaZahra-alien <fatimaijaz238@gmail.com>
2025-10-06 01:11:53 -0700

    hahahah

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://blogging-kappa-rosy.vercel.app', // <-- replace with your domain (or vercel URL for now)
+  generateRobotsTxt: true, // generate robots.txt file
+  sitemapSize: 7000,
+};
